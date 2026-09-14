19abe5b159c3555317226fd5ec70c534fe7e8099
aldur <1721633+aldur@users.noreply.github.com>
2026-09-14 11:56:32 +0000

    flake: bump llama-cpp

--- flake.nix
+++ flake.nix
@@ -34,12 +34,12 @@
           (
             finalAttrs: old: {
               # Optionally: bump to a more recent version than nixpkgs'
-              version = "10839";
+              version = "10955";
               src = pkgs.fetchFromGitHub {
                 owner = "ggml-org";
                 repo = "llama.cpp";
                 tag = "b${finalAttrs.version}";
-                hash = "sha256-GhaalIZFpoNuokOaOTKdzJOmQ44WRyQG2el/XLsAQ4A=";
+                hash = "sha256-/1QCLdQ1tuBwneJ2xanEtnXDu9GygdJ5tE4M/y4GYS4=";
                 leaveDotGit = true;
                 postFetch = ''
                   git -C "$out" rev-parse --short HEAD > $out/COMMIT
